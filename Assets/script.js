e414e9a740da7b194555080cdbb3781394e50952
aprilyanggarwood <yangyyj822@hotmail.com>
2020-10-18 01:37:23 -0700

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,140 @@
+// global variables
+
+let currentDay = moment();
+
+// Use JSON.parse() to convert the text in local storage into an object
+let searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
+
+renderHistory();
+$(document).ready(function () {
+  $("#submitCity").click((event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    getWeather();
+  });
+});
+
+// the current searched cities will be displayed as a button, and prepended to the previous searched cities
+function renderHistory() {
+  $("#searchedCities").empty();
+  searchedCities.forEach(function (city) {
+    $(
+      "#searchedCities"
+    ).prepend(`<button class="btn btn-lg mt-1 btn-primary" style="width: 80%">
+    ${city}
+    </button>`);
+  });
+}
+
+// click the searched city button, and the data of this serached city's weather will be gaven
+$("#searchedCities").click("button", function (event) {
+  getWeather(event.target.textContent);
+});
+
+// searched cities' data will be removed from local storage and displied area
+$("#clearHistory").click(function (event) {
+  event.preventDefault();
+  localStorage.removeItem("cities");
+  searchedCities = [];
+  renderHistory();
+});
+
+// request the date of current weather of each searched city from openweather API by jqury.ajax method
+function getWeather(city) {
+  let newCity = city || $("#inputCity").val();
+  if (newCity != "") {
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        newCity +
+        "&units=imperial&exclude=minutely,hourly&appid=809f1054aeccc4397ad67a0c43c8997f",
+      method: "GET",
+      dataType: "jsonp",
+      // save responsed cities to local storage as string by JSON.stringify method
+    }).then(function (response) {
+      if (!searchedCities.includes(response.name)) {
+        searchedCities.push(response.name);
+        localStorage.setItem("cities", JSON.stringify(searchedCities));
+        renderHistory();
+      }
+      console.log(response);
+      // displayed the current weather's data was grabed from the openweather API by gaven cities
+      $("#currentCitytWeather").html(`<h5 id="cityName-day-icon">
+             <span id="displayed-city">${response.name}
+        
+             </span>
+               <span id="today-date">
+               ${
+                 new Date(response.dt * 1000).getUTCMonth() + 1
+               }/${new Date(response.dt * 1000).getUTCDate()}/${new Date(response.dt * 1000).getUTCFullYear()}
+               
+               </span>
+               <span id="icon-img"><img src="https://openweathermap.org/img/wn/${
+                 response.weather[0].icon
+               }@2x.png">
+              </span>
+             </h5>
+             <p id="temp">${"Temperature：" + response.main.temp}</p>
+             <p id="humidity">${"Humidity：" + response.main.humidity + "%"}</p>
+             <p id="windSpeed">${
+               "Wind Speed：" + response.wind.speed + "MPH"
+             }</p>
+              <p>UV Index:<button class="btn btn-sm ml-2" id="uv-index"></button></p>
+             `);
+
+      // grab data from the openweather onecall API
+      $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${response.coord.lat}&lon=${response.coord.lon}&units=imperial&exclude=minutely,hourly&appid=809f1054aeccc4397ad67a0c43c8997f`,
+        method: "GET",
+        dataType: "jsonp",
+      }).then(function (data) {
+        console.log(data);
+        const uvi = data.current.uvi;
+
+        //adding uvi data to current weather
+        // the color of the uv index will be changed based on if the uv index is equal and less than 2 is considered favoralbe, if higher than 2 but equal and less than 8 is considered moderate, and if over 8 is considered severe.
+        $("#uv-index").text(data.current.uvi);
+        $("#uv-index").addClass(
+          uvi <= 2 ? "btn-primary" : uvi <= 8 ? "btn-warning" : "btn-danger"
+        );
+        // 5 days forcast data is displyed by for loop function
+        $("#forecast").html("");
+
+        for (let i = 1; i < 6; i++) {
+          $("#forecast").append(`
+          <div class="col-2">
+          <div
+          class="card text-white bg-info mb-3"
+          style="max-width: 48rem"
+          >
+          <div class="card-body">
+          <h5 class="card-title">${
+            new Date(data.daily[i].dt * 1000).getUTCMonth() + 1
+          }/${new Date(data.daily[i].dt * 1000).getUTCDate()}/${new Date(
+            data.daily[i].dt * 1000
+          ).getUTCFullYear()}</h5>
+          
+          <p id="icon-img"><img src="https://openweathermap.org/img/wn/${
+            data.daily[i].weather[0].icon
+          }@2x.png"></p>
+
+          <p class="card-text">${"Temperature：" + data.daily[i].temp.day}\n
+          ${"Humidity: " + data.daily[i].humidity}
+          </p>
+          
+          
+          </div>
+          </div>
+          </div>`);
+        }
+      });
+    });
+  }
+
+  {
+  }
+
+  // } else {
+  //   $("#error").html("<div>City field cannot be empty</div>");
+  // }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather Dashboard HW6( )
+
+## How this weather dashboard work for user:
+
+- The goal of this homework practice is to use OpenWeather API with retrieving data to create a weather dashboard with the following functions:
+
+  - accepted a user's request for a searched city,then gave a reponse with the data from OpenWeather API calls.
+
+  - the current searched citiy will be renderedd as a button with the city name under the search bar as well as prepended to the previous searched cities. Each searched city button has the function to render the weather API calls by executing the click event function.
+
+  - the data information for every searched city will be rended in the right of section which include the searched city name, the current day, the weather icon,the temperature, humidity, wind speed , UV index, and the 5 days forcast weather.
+
+  - the cities are searched will be stored in the local storage, and will be removed from the local storage by clicking the clear search button which is set up nearby the search button.
+
+  - the color of the UV index button will be changed based on if the UV index is equal and less than 2 is indicated the condition is favoralbe, if higher than 2 but equal and less than 8 is indicated the condition is moderate, and if over 8 is indicated the condition is severe. ( The reference for the UV index rang is from: https://www.epa.gov/sunsafety/uv-index-scale-0 )
+
+  ## Data from OpenWeather API
+
+  Two API calls were used to get the correct date. The Current Weather Data URL is used for getting the cities name and the current weather data. The One Call API URL is used for getting the lat and lon data, UV Index data , and the 5 days forcast data.
+
+  Jquary.\$.ajax() method was usded to make API calls and responses.
